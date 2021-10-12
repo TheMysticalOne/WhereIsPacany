1f54fc4893a01df58bc3c73369367088636ad3ec
TheMysticalOne <gavalentin@gmail.com>
2021-10-12 22:24:09 +0300

    Changes by yaroh ya tupoy

--- first_lesson.py
+++ first_lesson.py
@@ -1,4 +1,19 @@
+parta: int = 2
+clas = 15.124124
+pervyB = clas // parta  # + - / * %-остаток от деления //-целосичленное
+# print(pervyB)
+stroka2 = "PIDOR"  # строка с методами
+len(stroka2)  # длина строки
+bukva = stroka2[1]  # отдаст букву I из stroka2
 
+spisok = [1, 2, 23, 3, 45]
 
+# type()
 if __name__ == '__main__':
-    print("qwrqwrqwrqwrqr")
+    stroka2 = "qwrqwrqwr"
+    stroka3 = "ty-huila124"
+    s = "Valentin pidor nuhuya ne ponel"
+    cheslo = "12415"
+    #    v = s.replace('pidor', '')
+    # cheslo.isdigit()
+    print(int(cheslo)+46345745757)
