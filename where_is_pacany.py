cb0ee688cdc63411f08078a89dbc4422a4695027
TheMysticalOne <gavalentin@gmail.com>
2022-02-12 23:56:42 +0300

    + location request mechanism

--- where_is_pacany.py
+++ where_is_pacany.py
@@ -2,9 +2,12 @@ import random
 import re
 
 import telebot
+from telebot import types
 from telebot.types import Message
-
+from loguru import logger
 token = ''
+
+GROUP_ID =-1001188592066
 
 with open('token') as token_file:
     token = token_file.read()
@@ -41,18 +44,44 @@ def reply_to(message: Message, name: str):
 
     bot.reply_to(message, f'{prefix.strip().replace("{NAME}", name)} {case.strip()}')
 
+@bot.message_handler(commands=["loc"])
+def locs(message: Message):
+    try:
+        for admin in bot.get_chat_administrators(message.chat.id):
+            if admin.user.username == message.text.replace("/loc @",""):
+                keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+                button_geo = types.KeyboardButton(text="Я тут сука", request_location=True)
+                keyboard.add(button_geo)
+                bot.send_message(admin.user.id, "Ответь сука", reply_markup=keyboard)
+    except:
+        bot.send_message(message.chat.id , "SUKA YA NE MOGU NAYTI USERA V ADMINAH ILI ON MNE NE PISAL")
+
+@bot.message_handler(content_types=["location"])
+def forward(message):
+    if message.location is not None:
+        # bot.forward_message(to_chat_id, from_chat_id, message_id)
+        bot.forward_message(GROUP_ID, message.chat.id, message.id)
+        # logger.warning(message)
+        # logger.debug(message.location)
+        # logger.debug("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
+
 
 @bot.message_handler(func=lambda message: True)
 def gde_ros(message: Message):
-    with open('questions.txt', encoding='utf-8') as questions_file:
-        questions = questions_file.readlines()
-        for variant in questions:
-            txt = message.text.lower()
-            regex = variant.strip().replace("{NAME}", "(\\w+)")
-            name_found = re.findall(fr'(?u){regex}', txt, re.IGNORECASE)
-            if name_found:
-                reply_to(message, name_found[0])
-                return
+    try:
+        with open('questions.txt', encoding='utf-8') as questions_file:
+            questions = questions_file.readlines()
+            for variant in questions:
+                txt = message.text.lower()
+                regex = variant.strip().replace("{NAME}", "(\\w+)")
+                name_found = re.findall(fr'(?u){regex}', txt, re.IGNORECASE)
+                if name_found:
+                    reply_to(message, name_found[0])
+                    return
+    except:
+        bot.send_message(message.chat.id , "DAZHE YA HUI ZHAET GDE ETOT EBLAN")
+
+
 
 
 bot.infinity_polling()
