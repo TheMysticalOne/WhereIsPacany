6a6d63852bab6cb938416bc88f52f664303da53a
Tim Chernykh <tim.cherry.co@gmail.com>
2022-03-30 19:51:04 +0300

    pidor: blyat'

--- where_is_pacany.py
+++ where_is_pacany.py
@@ -56,9 +56,7 @@ def get_or_create_user_model(telegram_user: telebot.types.User, chat: telebot.ty
     user: UserModel = None
 
     try:
-        user = UserModel.get(
-            UserModel.chat_id == chat.id and UserModel.uname == telegram_user.username
-            or UserModel.uname == f"user:{telegram_user.username}")
+        user = UserModel.get(UserModel.chat_id == chat.id and UserModel.user_id == telegram_user.id)
     except:
         user = UserModel.create(
             uname=telegram_user.username if telegram_user.username else f"user:{telegram_user.first_name}",
@@ -76,8 +74,7 @@ def get_or_create_user_model_by_creds(username: str, user_id: int, chat_id: str,
     user: UserModel = None
 
     try:
-        user = UserModel.get(
-            UserModel.chat_id == chat_id and UserModel.uname == username or UserModel.uname == f"user:{username}")
+        user = UserModel.get(UserModel.chat_id == chat_id and UserModel.user_id == user_id)
 
         if user.user_id == user_id and user.uname != username:
             user.uname = username
